b969d1a1a3d83c4a11cff5d2674c0ff0831e50ee
Khevna-Patel <khevp@gmail.com>
2021-03-14 18:02:04 +0000

    Java homework

---
+++ src/Calculator1.java
@@ -0,0 +1,34 @@
+public class Calculator1 {
+    static int a=50;
+    static int b=10;
+    static int total;
+    static int subtraction;
+    static int multiplication;
+    static int division;
+
+    public static void main(String[] args){
+    total= a+b;
+    subtraction= a-b;
+    multiplication= a*b;
+    division= a/b;
+
+
+    System.out.println("Addition of " + a + " & " + b + " = "+total);
+    System.out.println("Subtraction of " + a + " & " + b + " = "+subtraction);
+    System.out.println("Multiplication of " + a + " & " + b +" = "+multiplication);
+    System.out.println("Division of " + a + " & " + b + " = "+division);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/PersonalDetails.java
@@ -0,0 +1,45 @@
+public class PersonalDetails {
+
+    public static void main(String[] args){
+        String name = "Khevna Patel";
+        String address ="32, Lewes Close, Northolt, Ub5 2QX";
+        String mobile ="07791196222";
+        String age ="34";
+        String securitycode ="35.60";
+        boolean eligibleforvote = true;
+        String grade ="b";
+
+        System.out.println("Name:"+name);
+        System.out.println("Address:"+address);
+        System.out.println("Mobile no:"+mobile);
+        System.out.println("Age:"+age);
+        System.out.println("Security code:"+securitycode);
+        System.out.println("Eligible for vote ="+eligibleforvote);
+        System.out.println("Grade:"+grade);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
